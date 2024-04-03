080aecc89ee5cbc96fe68c45c2d46b73b03440cd
Krissy Waters <krissyawaters@gmail.com>
2024-04-03 16:26:38 -0700

    initial

--- api-calls/routes/web.php
+++ api-calls/routes/web.php
@@ -1,20 +1,15 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\TvMazeAPI; 
+
+Route::get('/episodes', function () {
+    $showNumber = request('showNumber', 1); 
+    $episodes = TvMazeAPI::fetch($showNumber); 
+
+    return view('episodes.index', ['episodes' => $episodes]);
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
