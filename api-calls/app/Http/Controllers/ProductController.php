e4dfd8bc3eeb1ddc3f7105727a0add8753ecee4d
Krissy Waters <krissyawaters@gmail.com>
2024-04-20 11:59:15 -0700

    Generated controller for products

---
+++ api-calls/app/Http/Controllers/ProductController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();  
+        return view('products.index', compact('products'));  
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        /    $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|numeric',
+            'description' => 'nullable',
+            'item_number' => 'required|unique:products',
+            'image' => 'nullable|url'
+        ]);
+        Product::create($validatedData);
+        return redirect('/products');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'price' => 'required|numeric',
+            'description' => 'nullable',
+            'item_number' => 'unique:products,item_number,' . $product->id,
+            'image' => 'nullable|url'
+        ]);
+        $product->update($validatedData);
+        return redirect('/products');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect('/products');
+    }
+}
