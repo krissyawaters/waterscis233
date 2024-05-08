d0e39063c084ddbdd90cd3d9a46dbb250df3adb4
Krissy Waters <krissyawaters@gmail.com>
2024-05-08 12:10:20 -0700

    Crud-app file swap issue checking

---
+++ crud-app/crud_app/Models/Episode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Episode extends Model {
+  
+    protected $fillable = [
+        'name', 'image', 'season', 'episode', 'summary', 'show_number'
+    ];
+}
---
+++ crud-app/resources/views/episodes/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Episodes</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1 class="text-center mt-3">Episodes</h1>
+    <div class="container mt-5">
+        @if($episodes->isEmpty())
+            <div class="alert alert-info" role="alert">
+                There are no episodes to show.
+            </div>
+        @else
+            <div class="row">
+                @foreach($episodes as $episode)
+                    <div class="col-12 col-sm-6 col-md-4 mb-4">
+                        <div class="card h-100"> 
+                            @if($episode->image)
+                                <img src="{{ $episode->image }}" class="card-img-top" alt="Episode image">
+                            @endif
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $episode->name }}</h5>
+                                <p class="card-text">Season: {{ $episode->season }}, Episode: {{ $episode->episode }}</p>
+                                <p class="card-text">{{ Illuminate\Support\Str::limit(strip_tags($episode->summary), 150) }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        @endif
+    </div>
+</body>
+</html>
